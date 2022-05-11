3e7306fd2256f1d54e7284fa521ee7ebda05e78c
sashafll122 <alexandra_floru@brown.edu>
2022-05-11 16:23:13 -0400

    hi

--- src/main/java/edu/brown/cs/student/project1/MapGenerator.java
+++ src/main/java/edu/brown/cs/student/project1/MapGenerator.java
@@ -14,10 +14,10 @@ import java.util.Locale;
 public class MapGenerator {
 
     private HashMap<String, Integer> keyToInteger;
-    private HashMap<Integer, String> integerToKey;
 
 
-    private String userKey = new String();
+
+
 
     public MapGenerator(){
     }
@@ -31,53 +31,168 @@ public class MapGenerator {
 
     public HashMap<Integer, String> generateIntToKeyMap(String[] commandInputs){
 
-        integerToKey = new HashMap<>();
+        HashMap<Integer, String> integerToKey = new HashMap<>();
+        String userKey;
+
 
         System.out.println(commandInputs[2]);
-        userKey = commandInputs[2];
+        userKey = commandInputs[2].toUpperCase();
 
-        if (userKey.equals("c")){
+        if (userKey.equals("C")){
 
-            integerToKey.put(1, "c");
-            integerToKey.put(2, "d");
-            integerToKey.put(3, "e");
-            integerToKey.put(4, "f");
-            integerToKey.put(5, "g");
-            integerToKey.put(6, "a");
-            integerToKey.put(7, "b");
+            integerToKey.put(1, "C");
+            integerToKey.put(2, "D");
+            integerToKey.put(3, "E");
+            integerToKey.put(4, "F");
+            integerToKey.put(5, "G");
+            integerToKey.put(6, "A");
+            integerToKey.put(7, "B");
 
-        } else if (userKey.equals("d")){
+        } else if (userKey.equals("D")){
+
+            integerToKey.put(1, "D");
+            integerToKey.put(2, "E");
+            integerToKey.put(3, "F#");
+            integerToKey.put(4, "G");
+            integerToKey.put(5, "A");
+            integerToKey.put(6, "B");
+            integerToKey.put(7, "C#");
+
+        }else if (userKey.equals("E")){
+
+            integerToKey.put(1, "E");
+            integerToKey.put(2, "F#");
+            integerToKey.put(3, "G#");
+            integerToKey.put(4, "A");
+            integerToKey.put(5, "B");
+            integerToKey.put(6, "C#");
+            integerToKey.put(7, "D#");
+
+
+        }else if (userKey.equals("F")){
+
+            integerToKey.put(1, "F");
+            integerToKey.put(2, "G");
+            integerToKey.put(3, "A");
+            integerToKey.put(4, "Bb");
+            integerToKey.put(5, "C");
+            integerToKey.put(6, "D");
+            integerToKey.put(7, "E");
+
+        }else if (userKey.equals("G")){
+
+            integerToKey.put(1, "G");
+            integerToKey.put(2, "A");
+            integerToKey.put(3, "B");
+            integerToKey.put(4, "C");
+            integerToKey.put(5, "D");
+            integerToKey.put(6, "E");
+            integerToKey.put(7, "F#");
+
+        }else if (userKey.equals("A")){
+
+            integerToKey.put(1, "A");
+            integerToKey.put(2, "B");
+            integerToKey.put(3, "C#");
+            integerToKey.put(4, "D");
+            integerToKey.put(5, "E");
+            integerToKey.put(6, "F#");
+            integerToKey.put(7, "G#");
+
+        }else if (userKey.equals("B")){
+
+            integerToKey.put(1, "B");
+            integerToKey.put(2, "C#");
+            integerToKey.put(3, "D#");
+            integerToKey.put(4, "E");
+            integerToKey.put(5, "F#");
+            integerToKey.put(6, "G#");
+            integerToKey.put(7, "A#");
 
         }
         return integerToKey;
     }
 
-
-
-
     public HashMap<String, Integer> generateKeyToIntMap(String[] commandInputs){
 
-        keyToInteger = new HashMap<>();
+        HashMap<String, Integer> keyToInteger = new HashMap<>();
+        String userKey;
 
-        System.out.println(commandInputs[2]);
-        userKey = commandInputs[2];
+        userKey = commandInputs[2].toUpperCase();
 
-        if (userKey.equals("c")){
+        if (userKey.equals("C")){
 
-            keyToInteger.put("c", 1);
-            keyToInteger.put("d", 2);
-            keyToInteger.put("e", 3);
-            keyToInteger.put("f", 4);
-            keyToInteger.put("g", 5);
-            keyToInteger.put("a", 6);
-            keyToInteger.put("b", 7);
+            keyToInteger.put("C", 1);
+            keyToInteger.put("D", 2);
+            keyToInteger.put("E", 3);
+            keyToInteger.put("F", 4);
+            keyToInteger.put("G", 5);
+            keyToInteger.put("A", 6);
+            keyToInteger.put("B", 7);
 
             System.out.println("map " + keyToInteger);
 
-        } else if (userKey.equals("d")){
+        }else if (userKey.equals("D")){
+
+            keyToInteger.put("D", 1);
+            keyToInteger.put("E", 2);
+            keyToInteger.put("F#",3);
+            keyToInteger.put("G", 4);
+            keyToInteger.put("A", 5);
+            keyToInteger.put("B", 6);
+            keyToInteger.put("C#",7);
+
+        }else if (userKey.equals("E")){
+
+            keyToInteger.put("E", 1);
+            keyToInteger.put("F#", 2);
+            keyToInteger.put("G#", 3);
+            keyToInteger.put("A", 4);
+            keyToInteger.put("B", 5);
+            keyToInteger.put("C#", 6);
+            keyToInteger.put("D#", 7);
+
+        }else if (userKey.equals("F")){
+
+            keyToInteger.put("F", 1);
+            keyToInteger.put("G", 2);
+            keyToInteger.put("A", 3);
+            keyToInteger.put("Bb", 4);
+            keyToInteger.put("C", 5);
+            keyToInteger.put("D", 6);
+            keyToInteger.put("E", 7);
+
+        }else if (userKey.equals("G")){
+
+            keyToInteger.put("G", 1);
+            keyToInteger.put("A", 2);
+            keyToInteger.put("B", 3);
+            keyToInteger.put("C", 4);
+            keyToInteger.put("D", 5);
+            keyToInteger.put("E", 6);
+            keyToInteger.put("F#", 7);
+
+        }else if (userKey.equals("A")){
+
+            keyToInteger.put("A", 1);
+            keyToInteger.put("B", 2);
+            keyToInteger.put("C#", 3);
+            keyToInteger.put("D", 4);
+            keyToInteger.put("E", 5);
+            keyToInteger.put("F#", 6);
+            keyToInteger.put("G#", 7);
+
+        }else if (userKey.equals("B")){
+
+            keyToInteger.put("B", 1);
+            keyToInteger.put("C#", 2);
+            keyToInteger.put("D#", 3);
+            keyToInteger.put("E", 4);
+            keyToInteger.put("F#", 5);
+            keyToInteger.put("G#", 6);
+            keyToInteger.put("A#", 7);
 
         }
-
         return keyToInteger;
 
     }
--- src/main/java/edu/brown/cs/student/project1/PopRecommender.java
+++ src/main/java/edu/brown/cs/student/project1/PopRecommender.java
@@ -10,11 +10,14 @@ public class PopRecommender implements Recommender{
 
    MapGenerator _mapGenerator;
    HashMap<String, Integer> _generatedMap;
+   HashMap<Integer, String> _intToString;
 
     //chord progressions that will be used as a baseline for the recommendations
     Integer[] _chordProgression1 = {1, 4, 5, 4};
     Integer[] _chordProgression2 = {1, 5, 6, 4};
     Integer[] _chordProgression3 = {1, 6, 4, 5};
+
+    //idea: check for entire;y equivalent chord inputs and only run more random algorithm if there is not complete equivalence
 
     String _firstChord;
     String _secondChord;
@@ -28,11 +31,6 @@ public class PopRecommender implements Recommender{
     int _thirdChordInt;
     int _fourthChordInt;
 
-
-    int _chordProg1SimilarityIndex = 0;
-    int _chordProg2SimilarityIndex = 0;
-    int _chordProg3SimilarityIndex = 0;
-
     /**
      * Recommender will return as many notes to fill a bar
      * @param commandInputs inputted genre, key, and four chords
@@ -41,23 +39,26 @@ public class PopRecommender implements Recommender{
     @Override
     public void recommend(String[] commandInputs) {
 
+        int _chordProg1SimilarityIndex = 0;
+        int _chordProg2SimilarityIndex = 0;
+        int _chordProg3SimilarityIndex = 0;
+
         this._firstChord = commandInputs[3];
         this._secondChord = commandInputs[4];
         this._thirdChord = commandInputs[5];
         this._fourthChord = commandInputs[6];
 
         this._generatedMap = new HashMap<>();
+        this._intToString = new HashMap<>();
 
     //System.out.println("genre: " +commandInputs[1] + "  key: "+ commandInputs[2] + "  chord 1: "+ commandInputs[3] + "  chord 2: "+  commandInputs[4] + "  chord 3: "+  commandInputs[5] + "  chord 4: "+  commandInputs[6]);
 
 
         this._mapGenerator = new MapGenerator();
-        this._generatedMap = this._mapGenerator.generateKeyToIntMap(commandInputs);
 
-    //System.out.println("first chord number " + this._generatedMap.get(this._firstChord));
-    //System.out.println("second chord number " + this._generatedMap.get(this._secondChord));
-    //System.out.println("third chord number " + this._generatedMap.get(this._thirdChord));
-    //System.out.println("fourth chord number " + this._generatedMap.get(this._fourthChord));
+        //REFACTOR
+        this._generatedMap = this._mapGenerator.generateKeyToIntMap(commandInputs);
+        this._intToString = this._mapGenerator.generateIntToKeyMap(commandInputs);
 
     _matchList = new ArrayList<>();
 
@@ -89,7 +90,7 @@ public class PopRecommender implements Recommender{
         for (int i = 0; i < this._chordProgression1.length ; i++){
             for (int j = 0; j < this._matchList.size(); j++){
             if (this._matchList.get(j).equals(this._chordProgression1[i])){
-                this._chordProg1SimilarityIndex = this._chordProg1SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
+                _chordProg1SimilarityIndex = _chordProg1SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
                                                                              // set chord progressions and the user's desired chords.
                 }
             }
@@ -98,7 +99,7 @@ public class PopRecommender implements Recommender{
         for (int i = 0; i < this._chordProgression2.length ; i++){
             for (int j = 0; j < this._matchList.size(); j++){
                 if (this._matchList.get(j).equals(this._chordProgression2[i])){
-                    this._chordProg2SimilarityIndex = this._chordProg2SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
+                    _chordProg2SimilarityIndex = _chordProg2SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
                     // set chord progressions and the user's desired chords.
                 }
             }
@@ -107,7 +108,7 @@ public class PopRecommender implements Recommender{
         for (int i = 0; i < this._chordProgression3.length ; i++){
             for (int j = 0; j < this._matchList.size(); j++){
                 if (this._matchList.get(j).equals(this._chordProgression3[i])){
-                    this._chordProg3SimilarityIndex = this._chordProg3SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
+                    _chordProg3SimilarityIndex = _chordProg3SimilarityIndex + 1; //increment similarity checker by 1 for ever common chord between the
                     // set chord progressions and the user's desired chords.
                 }
             }
@@ -119,41 +120,53 @@ public class PopRecommender implements Recommender{
       //while loop to ensure incrementing doesn't result in repeated equivalence (for example in case that similarity indices are 2, 2, 3)
       //this.chordSimilarityIndexRandomChoice(this._chordProg1SimilarityIndex, this._chordProg2SimilarityIndex, this._chordProg3SimilarityIndex);
 
-        if (this._chordProg1SimilarityIndex == this._chordProg2SimilarityIndex){
-            this._chordProg1SimilarityIndex++ ;
-        }
-        if (this._chordProg1SimilarityIndex == this._chordProg3SimilarityIndex){
-            this._chordProg3SimilarityIndex++;
-        }
-        if ( this._chordProg2SimilarityIndex == this._chordProg3SimilarityIndex){
-            this._chordProg2SimilarityIndex++;
+
+        while (_chordProg1SimilarityIndex == _chordProg3SimilarityIndex){
+            _chordProg3SimilarityIndex = _chordProg3SimilarityIndex + (int)(Math.random()*7);
+            System.out.println("adding 3");
+
         }
 
-        System.out.println(" chord 1 similar: " + this._chordProg1SimilarityIndex);
-        System.out.println(" chord 2 similar: " + this._chordProg2SimilarityIndex);
-        System.out.println(" chord 3 similar: " + this._chordProg3SimilarityIndex);
+        while (_chordProg1SimilarityIndex == _chordProg2SimilarityIndex || _chordProg2SimilarityIndex == _chordProg3SimilarityIndex){
+            _chordProg1SimilarityIndex = _chordProg1SimilarityIndex + (int)(Math.random()*7) ;
+            _chordProg2SimilarityIndex = _chordProg2SimilarityIndex + (int)(Math.random()*7);
+            System.out.println("adding 1");
+            System.out.println("adding 2");
 
-
-        if (this._chordProg2SimilarityIndex > this._chordProg1SimilarityIndex){
-
-            if (this._chordProg2SimilarityIndex > this._chordProg3SimilarityIndex){
-                secondChordProgRecommender();
-            }
-
-            if (this._chordProg2SimilarityIndex < this._chordProg3SimilarityIndex ){
-                thirdChordProgRecommender();
-            }
         }
 
-        if (this._chordProg2SimilarityIndex < this._chordProg1SimilarityIndex){
 
 
-            if (this._chordProg1SimilarityIndex > this._chordProg3SimilarityIndex){
+
+        System.out.println(" chord 1 similar: " + _chordProg1SimilarityIndex);
+        System.out.println(" chord 2 similar: " + _chordProg2SimilarityIndex);
+        System.out.println(" chord 3 similar: " + _chordProg3SimilarityIndex);
+
+
+        if (_chordProg2SimilarityIndex > _chordProg1SimilarityIndex){
+
+            if (_chordProg2SimilarityIndex > _chordProg3SimilarityIndex){
+
                 firstChordProgRecommender();
+                //secondChordProgRecommender();
             }
 
-            if (this._chordProg1SimilarityIndex < this._chordProg3SimilarityIndex ){
-                thirdChordProgRecommender();
+            if (_chordProg2SimilarityIndex < _chordProg3SimilarityIndex ){
+
+                firstChordProgRecommender();
+                // thirdChordProgRecommender();
+            }
+        }
+
+
+        if (_chordProg2SimilarityIndex < _chordProg1SimilarityIndex){
+
+            if (_chordProg1SimilarityIndex > _chordProg3SimilarityIndex){
+                this.firstChordProgRecommender();
+            }
+
+            if (_chordProg1SimilarityIndex < _chordProg3SimilarityIndex ){
+                this.thirdChordProgRecommender();
             }
 
         }
@@ -161,23 +174,104 @@ public class PopRecommender implements Recommender{
     }
 
 
-    public void firstChordProgRecommender(){
+    //Integer[] _chordProgression1 = {1, 4, 5, 4};
+    public String firstChordProgRecommender(){
+
+        double _barCounter;
+        String _noteRhythm;
+        String _note;
+        String _longOutputNoteString = "";
 
         System.out.println("Recommending based on Chord Progression 1");
 
+        _barCounter = 0;
+
+        while (_barCounter < 8){
+
+            for (int i = 0; i < _chordProgression1.length ; i ++){
+
+                _noteRhythm = generateRhythm();
+
+                _note = _intToString.get(_chordProgression1[i]);
+
+
+                _note.concat(_noteRhythm);
+               // System.out.println(_note + _noteRhythm);
+                _longOutputNoteString = _longOutputNoteString + _note.concat(_noteRhythm);
+
+                //keeps track of whether enough/too many notes have been generated
+                if (_noteRhythm.equals("q")) {
+                    _barCounter = _barCounter + 1;
+                } else if(_noteRhythm.equals("8")){
+                    _barCounter = _barCounter + 0.5;
+                } else if(_noteRhythm.equals("h")){
+                    _barCounter = _barCounter + 2;
+                } else if (_noteRhythm.equals("w")) {
+                    _barCounter = _barCounter + 4;
+                }
+
+            }
+
+        }
+        System.out.println(_longOutputNoteString);
+        return _longOutputNoteString;
+
     }
 
+
+
+
+    //Integer[] _chordProgression2 = {1, 5, 6, 4};
     public void secondChordProgRecommender(){
-
         System.out.println("Recommending based on Chord Progression 2");
-
     }
 
+
+    //Integer[] _chordProgression3 = {1, 6, 4, 5};
     public void thirdChordProgRecommender(){
-
         System.out.println("Recommending based on Chord Progression 3");
+    }
+
+
+
+
+
+    String rhythm;
+    public String generateRhythm() {
+
+        int rand_int = (int) (Math.random() * 3);
+
+        switch (rand_int) {
+
+            //eighth note
+            case 0 :
+                rhythm = "8" ;
+                break;
+
+            //sixteenth note
+            case 1 :
+                rhythm = "h" ;
+                break;
+
+            //whole note
+            case 2 :
+                rhythm = "w";
+                break;
+
+            default:
+                rhythm = "q";
+                break;
+        }
+
+    return rhythm;
 
     }
+
+
+
+
+
+
 
 
 
